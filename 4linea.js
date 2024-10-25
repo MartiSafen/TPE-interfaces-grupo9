fa6999c1f9c74e22dfc9c91004b3eb3e3ec74816
Marcos Gallinal <gallinalmarcos@gmail.com>
2024-10-25 17:18:20 -0300

    empezando 4enlinea

---
+++ 4linea.js
@@ -0,0 +1,8 @@
+// ------------------------ 4 en linea --------------------------------------------
+
+const canvas = document.querySelector("#myCanvas");
+const ctx = canvas.getContext("2d");
+
+
+ctx.fillRect(100,100,100,100);
+
